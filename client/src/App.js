f4e10768969066c9850963a4d4ea6ede4d9c735a
BiniTutor <binitutor1@gmail.com>
2023-09-15 13:36:06 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flower: {}
+    }
+    
+  }
+  componentWillMount() {
+    this.getFlower();
+  }
+  getFlower() {
+    fetch('/flower')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          flower: data
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.flower.name}</h1>
+        <p>{this.state.flower.colour}</p>
+      </div>
+    );
+  }
+}
+export default App;
